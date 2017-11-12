df912f60ffb62c0a4fa803cc6aece0f62a521fee
Darth-koder007 <vj2010sn@gmail.com>
2017-11-12 19:54:45 +0530

    search list view complete

--- src/components/search_result_list.js
+++ src/components/search_result_list.js
@@ -17,8 +17,40 @@ import React, { Component } from 'react';
 
       render() {
           return (
-              <div>
-                  Search Result List
+              <div className="search-list-container">
+                <div className="heading-container">
+                    <p className="heading">Search results for "Jack". <span>( clear )</span></p>
+                </div>
+                <ul className="search-results-list">
+                    <li className="list-item">
+                        <div className="list-item-container">
+                            <img src="http://via.placeholder.com/150x150" />
+                            <div className="details-container">
+                                <p className="heading">Artist name: Francis Ford coppolla</p>
+                                <p className="heading">Track name: Jack</p>
+                                <p className="sub-heading">
+                                    Deserunt Lorem laboris magna id nostrud culpa anim labore ea non adipisicing do. Anim in duis minim nisi quis commodo duis do quis. 
+                                    Enim occaecat sint nulla minim sint dolor reprehenderit excepteur sint nisi sint. Labore sit veniam elit sint. Commodo non Lorem 
+                                    proident tempor esse culpa. Ex ipsum incididunt nulla ex Lorem laboris anim ea.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                    <li className="list-item">
+                        <div className="list-item-container">
+                            <img src="http://via.placeholder.com/150x150" />
+                            <div className="details-container">
+                                <p className="heading">Artist name: Francis Ford coppolla</p>
+                                <p className="heading">Track name: Jack</p>
+                                <p className="sub-heading">
+                                    Deserunt Lorem laboris magna id nostrud culpa anim labore ea non adipisicing do. Anim in duis minim nisi quis commodo duis do quis. 
+                                    Enim occaecat sint nulla minim sint dolor reprehenderit excepteur sint nisi sint. Labore sit veniam elit sint. Commodo non Lorem 
+                                    proident tempor esse culpa. Ex ipsum incididunt nulla ex Lorem laboris anim ea.
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
               </div>
           );
       }
